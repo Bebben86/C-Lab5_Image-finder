c6bbc8a2760676f03c8f9b3082aa87ba3663979a
Andreas Nilsson <andreas.j.nilsson@outlook.com>
2021-03-24 13:53:10 +0100

    Add project files.

---
+++ Forms/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_URL = new System.Windows.Forms.TextBox();
+            this.btn_Go = new System.Windows.Forms.Button();
+            this.lb_Result = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_SelectAll = new System.Windows.Forms.Button();
+            this.lbl_NumberOfFoundImages = new System.Windows.Forms.Label();
+            this.btn_ClearSelection = new System.Windows.Forms.Button();
+            this.btn_DownloadImages = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // tb_URL
+            // 
+            this.tb_URL.Location = new System.Drawing.Point(82, 28);
+            this.tb_URL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_URL.Name = "tb_URL";
+            this.tb_URL.Size = new System.Drawing.Size(344, 23);
+            this.tb_URL.TabIndex = 1;
+            // 
+            // btn_Go
+            // 
+            this.btn_Go.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Go.Location = new System.Drawing.Point(474, 28);
+            this.btn_Go.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Go.Name = "btn_Go";
+            this.btn_Go.Size = new System.Drawing.Size(82, 36);
+            this.btn_Go.TabIndex = 2;
+            this.btn_Go.Text = "Go!";
+            this.btn_Go.UseVisualStyleBackColor = true;
+            this.btn_Go.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb_Result
+            // 
+            this.lb_Result.BackColor = System.Drawing.SystemColors.Info;
+            this.lb_Result.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_Result.FormattingEnabled = true;
+            this.lb_Result.HorizontalScrollbar = true;
+            this.lb_Result.ItemHeight = 15;
+            this.lb_Result.Location = new System.Drawing.Point(6, 29);
+            this.lb_Result.Name = "lb_Result";
+            this.lb_Result.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lb_Result.Size = new System.Drawing.Size(774, 289);
+            this.lb_Result.TabIndex = 4;
+            this.lb_Result.SelectedIndexChanged += new System.EventHandler(this.lb_Result_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.btn_SelectAll);
+            this.groupBox1.Controls.Add(this.lbl_NumberOfFoundImages);
+            this.groupBox1.Controls.Add(this.btn_ClearSelection);
+            this.groupBox1.Controls.Add(this.btn_DownloadImages);
+            this.groupBox1.Controls.Add(this.lb_Result);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(42, 68);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(786, 435);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "IMG URLs";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btn_SelectAll
+            // 
+            this.btn_SelectAll.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_SelectAll.Location = new System.Drawing.Point(646, 353);
+            this.btn_SelectAll.Name = "btn_SelectAll";
+            this.btn_SelectAll.Size = new System.Drawing.Size(134, 23);
+            this.btn_SelectAll.TabIndex = 8;
+            this.btn_SelectAll.Text = "Select all";
+            this.btn_SelectAll.UseVisualStyleBackColor = true;
+            this.btn_SelectAll.Click += new System.EventHandler(this.btn_SelectAll_Click);
+            // 
+            // lbl_NumberOfFoundImages
+            // 
+            this.lbl_NumberOfFoundImages.AutoSize = true;
+            this.lbl_NumberOfFoundImages.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_NumberOfFoundImages.Location = new System.Drawing.Point(6, 327);
+            this.lbl_NumberOfFoundImages.Name = "lbl_NumberOfFoundImages";
+            this.lbl_NumberOfFoundImages.Size = new System.Drawing.Size(20, 17);
+            this.lbl_NumberOfFoundImages.TabIndex = 7;
+            this.lbl_NumberOfFoundImages.Text = "   ";
+            // 
+            // btn_ClearSelection
+            // 
+            this.btn_ClearSelection.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_ClearSelection.Location = new System.Drawing.Point(646, 324);
+            this.btn_ClearSelection.Name = "btn_ClearSelection";
+            this.btn_ClearSelection.Size = new System.Drawing.Size(134, 23);
+            this.btn_ClearSelection.TabIndex = 6;
+            this.btn_ClearSelection.Text = "Clear selection";
+            this.btn_ClearSelection.UseVisualStyleBackColor = true;
+            this.btn_ClearSelection.Click += new System.EventHandler(this.btn_ClearSelection_Click);
+            // 
+            // btn_DownloadImages
+            // 
+            this.btn_DownloadImages.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_DownloadImages.Location = new System.Drawing.Point(646, 382);
+            this.btn_DownloadImages.Name = "btn_DownloadImages";
+            this.btn_DownloadImages.Size = new System.Drawing.Size(134, 23);
+            this.btn_DownloadImages.TabIndex = 5;
+            this.btn_DownloadImages.Text = "Download selected";
+            this.btn_DownloadImages.UseVisualStyleBackColor = true;
+            this.btn_DownloadImages.Click += new System.EventHandler(this.btn_DownloadImages_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 527);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_Go);
+            this.Controls.Add(this.tb_URL);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Image finder";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_URL;
+        private System.Windows.Forms.Button btn_Go;
+        private System.Windows.Forms.ListBox lb_Result;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_DownloadImages;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btn_ClearSelection;
+        private System.Windows.Forms.Label lbl_NumberOfFoundImages;
+        private System.Windows.Forms.Button btn_SelectAll;
+    }
+}
+
---
+++ Forms/Form1.cs
@@ -0,0 +1,178 @@
+﻿using Htmlhelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// ----------------------------------------------------------------------------------------------------
+// |                                This is Lab5 by Andreas Nilsson                                   |
+// ----------------------------------------------------------------------------------------------------
+
+
+namespace Forms
+{
+    public partial class Form1 : Form
+    {
+
+
+        public List<string> resultList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e) //GO-button
+        {
+            string input = tb_URL.Text;
+            if (input == "" || input == " ")
+            {
+                MessageBox.Show("URL can't be empty.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tb_URL.Text = "";
+                return;
+            }
+
+            if (!Uri.IsWellFormedUriString(input, UriKind.Absolute))
+            {
+                MessageBox.Show("Invalid URL. Check and try again.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tb_URL.Text = input;                            //Keeps the previous input after warning message is shown.
+                return;
+            }
+            HtmlExtract extract = new HtmlExtract();
+            resultList = await extract.CallURL(input);
+
+            if (resultList.Count <= 0)
+            {
+                MessageBox.Show("No images found.", "Zero images", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                for (int i = 0; i < resultList.Count; i++)
+                {
+                    if (resultList[i].Contains("http://") || resultList[i].Contains("https://"))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        resultList[i] = tb_URL.Text + resultList[i];
+                    }
+                }
+
+                lb_Result.DataSource = resultList;
+                lbl_NumberOfFoundImages.Visible = true;
+                lbl_NumberOfFoundImages.Text = $"{resultList.Count} images found";
+
+            }
+
+        }
+
+        private void btn_ClearSelection_Click(object sender, EventArgs e)
+        {
+            lb_Result.ClearSelected();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_SelectAll_Click(object sender, EventArgs e)
+        {
+            //Varför fungerar inte lb_Result.SelectAll(); ???
+            for (int i = 0; i < lb_Result.Items.Count; i++)
+            {
+                lb_Result.SetSelected(i, true);
+            }
+
+        }
+
+        private async void btn_DownloadImages_ClickAsync(object sender, EventArgs e)
+        {
+            var selectedPics = lb_Result.SelectedItems.Cast<string>().ToList();
+            if (lb_Result.SelectedItems.Count > 0)
+            {
+
+                var client = new HttpClient();
+                Dictionary<Task<byte[]>, string> downloads = new Dictionary<Task<byte[]>, string>(); //Container for imagebytes
+                string folder = FolderPath();
+                int numOfPics = 0;
+                int downloadedPics = 0;
+
+                foreach (var url in selectedPics)
+                {
+                    string fileExtension = Regex.Match(url, @"(?<=\.)(jpg|jpeg|png|gif|bmp)(?=(""|\?|$))").Value;
+                    downloads.Add(Task.Run(() => client.GetByteArrayAsync(url)), fileExtension);
+                }
+                if (downloads != null)
+                {
+
+
+                    while (downloads.Count > 0)
+                    {
+                        Task<byte[]> task = await Task.WhenAny(downloads.Keys);
+
+                        numOfPics++;
+                        if (task.Exception == null)
+                        {
+
+                            string fileExtension = downloads[task];
+                            string fileName = $"Image{numOfPics,000}.{fileExtension}";
+                            string fullPath = Path.Combine(folder, fileName);
+                            await SaveImage(fullPath, task.Result);
+                            downloadedPics++;
+                            lbl_NumberOfFoundImages.Text = "Downloaded images: " + Convert.ToString(downloadedPics) + "/" + Convert.ToString(selectedPics.Count);
+                        }
+                        downloads.Remove(task);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+        }
+
+        private async Task SaveImage(string path, byte[] data)
+        {
+            var fs = new FileStream(path, FileMode.Create);
+            await fs.WriteAsync(data, 0, data.Length);
+            fs.Close();
+        }
+        public string FolderPath()
+        {
+            string folder = "";
+            using (var folderBrowser = new FolderBrowserDialog())
+            {
+                DialogResult dialog = folderBrowser.ShowDialog();
+                if (dialog == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowser.SelectedPath))
+                {
+                    folder = folderBrowser.SelectedPath;
+                    return folder;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        void wc_DownloadFilesCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Selected file(s) downloaded");
+        }
+
+        private void lb_Result_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
